7bb73538173c61d668544543a1dbea4b18e6f99c
Karthik P <karthik.p@talview.com>
2016-11-23 13:34:16 +0530

    Added schema for forgot password key and saving items

---
+++ console/migrations/m161123_073540_misc_stuff.php
@@ -0,0 +1,47 @@
+<?php
+
+use yii\db\Migration;
+
+class m161123_073540_misc_stuff extends Migration
+{
+    public function safeUp()
+    {
+        $this->addColumn('AccountInfo', 'forgot_pass_key', $this->text());
+        $this->createTable('item',[
+            'id'                                => $this->primaryKey(),
+            'second_column_id'                  => $this->integer()->defaultValue(35)->notNull(),
+            'is_available_eshop'                => $this->boolean()->defaultValue('FALSE')->notNull(),
+            'name'                              => $this->text()->notNull(),
+            'type'                              => $this->integer()->defaultValue(1)->notNull(),
+            'meta'                              => $this->text(),
+            'created_at'                        => $this->dateTime() . " DEFAULT CURRENT_TIMESTAMP",
+            'updated_at'                        => $this->dateTime() . " DEFAULT CURRENT_TIMESTAMP"
+        ]);
+        $this->createTable('item_info',[
+            'id'                                => $this->primaryKey(),
+            'item_id'                           => $this->integer()->notNull(),
+            'level'                             => $this->integer()->defaultValue(1)->notNull(),
+            'strength'                          => $this->integer()->defaultValue(25)->notNull(),
+            'intelligence'                      => $this->integer()->defaultValue(25)->notNull(),
+            'dexterity'                         => $this->integer()->defaultValue(25)->notNull(),
+            'damage'                            => $this->integer()->defaultValue(25)->notNull(),
+            'incremental_damage'                => $this->integer()->defaultValue(10)->notNull(),
+            'max_ice_elements'                  => $this->integer()->defaultValue(10)->notNull(),
+            'max_fire_elements'                 => $this->integer()->defaultValue(10)->notNull(),
+            'max_light_elements'                => $this->integer()->defaultValue(10)->notNull(),
+            'additional_stats'                  => $this->integer()->defaultValue(2)->notNull(),
+            'meta'                              => $this->text(),
+            'created_at'                        => $this->dateTime() . " DEFAULT CURRENT_TIMESTAMP",
+            'updated_at'                        => $this->dateTime() . " DEFAULT CURRENT_TIMESTAMP"
+        ]);
+        $this->addForeignKey('fk_item_level_item', 'item_level', 'item_id', 'item', 'id');
+    }
+
+    public function safeDown()
+    {
+        $this->dropForeignKey('fk_item_level_item', 'item_level');
+        $this->dropTable('item_level');
+        $this->dropTable('item');
+        $this->dropColumn('AccountInfo', 'forgot_pass_key');
+    }
+}
