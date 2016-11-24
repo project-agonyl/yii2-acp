2744b13d782abf5a8710b9b1fc676f0d18fb2041
Karthik P <karthikreadnovel@gmail.com>
2016-11-24 22:53:51 +0530

    Importing IT1, IT2 and IT3 text files

--- console/controllers/ImportController.php
+++ console/controllers/ImportController.php
@@ -31,6 +31,9 @@ class ImportController extends Controller
         return array_merge(parent::options($id), $options);
     }
 
+    /**
+     * Import IT0.txt file into database
+     */
     public function actionIt0()
     {
         if ($this->path == null) {
@@ -66,6 +69,48 @@ class ImportController extends Controller
                 } else {
                     Console::error('Item '.$item->item_id.' failed.');
                 }
+            }
+            $currentLine++;
+        }
+        fclose($fp);
+    }
+
+    /**
+     * Import IT1.txt or IT2.txt or IT3.txt file into database
+     */
+    public function actionIt123()
+    {
+        if ($this->path == null) {
+            $this->path =  Console::prompt('Please the path of the file to import:', ['required' => true]);
+        }
+        if (!file_exists($this->path)) {
+            Console::error('File does not exists or you do not have the permission to access it!');
+            return;
+        }
+        $fp = fopen($this->path, 'r');
+        $currentLine = 0;
+        while(!feof($fp)){
+            $line = fgets($fp);
+            if ($currentLine == 0) {
+                $currentLine++;
+                continue;
+            }
+            $temp = explode(',', mb_convert_encoding($line, 'ASCII'));
+            $item = Item::find()
+                ->where(['item_id' => (int)$temp[0]])
+                ->one();
+            if ($item == null) {
+                $item = new Item();
+                $item->item_id = (int)$temp[0];
+                $item->type = (int)$temp[1];
+            }
+            $item->name = trim($temp[3]);
+            $item->woonz = (int)$temp[4];
+            $item->meta = Json::encode($temp);
+            if ($item->save()) {
+                Console::output('Successfully saved item '.$item->item_id);
+            } else {
+                Console::error('Item '.$item->item_id.' failed.');
             }
             $currentLine++;
         }
