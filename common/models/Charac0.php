bbb07e66139ff95401276b28171bf6048c26cc4e
Karthik P <karthikreadnovel@gmail.com>
2016-11-25 00:39:40 +0530

    Showing of character invetory and wear in backend

--- common/models/Charac0.php
+++ common/models/Charac0.php
@@ -15,6 +15,8 @@ class Charac0 extends BaseCharac0
 {
     const STATUS_ACTIVE = 'A';
 
+    protected $_itemModels = [];
+
     public function behaviors()
     {
         return ArrayHelper::merge(
@@ -62,5 +64,84 @@ class Charac0 extends BaseCharac0
                 break;
         }
         return $type;
+    }
+
+    public function getParsedInventory()
+    {
+        $inventoryString = ArrayHelper::getValue(explode('\_1', $this->m_body), 6);
+        if ($inventoryString == null) {
+            return [];
+        }
+        $temp = explode('=', $inventoryString);
+        $itemArray = explode(';', $temp[1]);
+        $toReturn = [];
+        for ($i = 0; $i < count($itemArray);$i += 4) {
+            $toReturn[(int)$itemArray[$i + 3]] = $this->processItem(
+                (int)$itemArray[$i],
+                (int)$itemArray[$i + 1],
+                (int)$itemArray[$i + 2]
+            );
+        }
+        return $toReturn;
+    }
+
+    public function getParsedWear()
+    {
+        $wearString = ArrayHelper::getValue(explode('\_1', $this->m_body), 5);
+        if ($wearString == null) {
+            return [];
+        }
+        $temp = explode('=', $wearString);
+        $itemArray = explode(';', $temp[1]);
+        $toReturn = [];
+        for ($i = 0; $i < count($itemArray);$i += 3) {
+            $toReturn[] = $this->processItem(
+                (int)$itemArray[$i],
+                (int)$itemArray[$i + 1],
+                (int)$itemArray[$i + 2]
+            );
+        }
+        return $toReturn;
+    }
+
+    public function processItem($column1, $column2, $column3)
+    {
+        $toReturn['original_item_code'] = implode(';', [$column1, $column2, $column3]);
+        $mounted = 0;
+        $bless = false;
+        while ($column1 > 65536) {
+            $mounted++;
+            $column1 -= 65536;
+        }
+        if ($column1 > 32768) {
+            $bless = true;
+            $column1 -= 32768;
+        }
+        if (!isset($this->_itemModels[$column1])) {
+            $item = Item::find()
+                ->where(['item_id' => $column1])
+                ->one();
+            if ($item == null) {
+                $toReturn['item_name'] = 'Unknown Item';
+                return $toReturn;
+            }
+            $this->_itemModels[$column1] = $item;
+        } else {
+            $item = $this->_itemModels[$column1];
+        }
+        $toReturn['item_name'] = $item->name;
+        $toReturn['item_id'] = $item->item_id;
+        $toReturn['full_item_name'] = $item->name;
+        if ($mounted != 0) {
+            $toReturn['full_item_name'] = ($mounted*10).'% Mounted '.$toReturn['full_item_name'];
+        }
+        if ($bless) {
+            $toReturn['full_item_name'] = 'Blessed '.$toReturn['full_item_name'];
+        }
+        $toReturn['options'] = [
+            'mount' => $mounted,
+            'bless' => $bless
+        ];
+        return $toReturn;
     }
 }
