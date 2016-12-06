a88c4b30ea99db13d65a4cfd9030aaceca990f79
Karthik SG <Karthik SG>
2016-12-06 14:27:45 +0530

    Updated template

--- common/mail/templates/body/7.php
+++ common/mail/templates/body/7.php
@@ -1,5 +1,5 @@
 <p>Dear old player of A3Flamez,</p>
-<p>A request raised was to convert items in your old A3Flamez account to Flamez Coins.</p>
+<p>A request was raised to convert items in your old A3Flamez account to Flamez Coins.</p>
 <p>Old Account Username: <b><?= $old_account; ?></b></p>
 <p>New Account Username: <b><?= $new_account; ?></b></p>
 <p>If you want to authorize this transfer please click on the link below</p>
