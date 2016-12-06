640b8659a793087fc90d9fc2b5e28c105f64ab40
Karthik SG <Karthik SG>
2016-12-06 14:33:16 +0530

    Updated activation mail

--- common/mail/templates/body/1.php
+++ common/mail/templates/body/1.php
@@ -5,5 +5,8 @@
 <p>Password: <b><?= $password ?></b></p>
 <p>Please activate your account by clicking the following.</p>
 <p><a href="<?= $activationLink ?>"><?= $activationLink ?></a></p>
+<p>
+To Play the game, download the <a href="https://drive.google.com/open?id=0B9GLPwCwizw5Ry11a1pMNEVwbkU">full client</a> or just the <a href="http://www.mediafire.com/file/iafjrbgwo6a3vll/A3FlamezLauncher.exe">Updater(recommended)</a>
+</p>
 <p>Regards,</p>
 <p>A3Flamez Team</p>
