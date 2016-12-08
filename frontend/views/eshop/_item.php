4d49dac58cd558cc0e5741125355f3206dec48da
Karthik P <karthik.p@talview.com>
2016-12-08 16:16:42 +0530

    Minor UI fix for showing e-shop items

--- frontend/views/eshop/_item.php
+++ frontend/views/eshop/_item.php
@@ -7,13 +7,13 @@ use kartik\icons\Icon;
 use yii\helpers\Url;
 
 ?>
-<div class="col-sm-4 col-lg-4 col-md-4">
+<div class="col-sm-3 col-lg-3 col-md-3">
     <div class="thumbnail">
         <img src="http://placehold.it/320x150" alt="">
         <div class="caption">
-            <h4 style="cursor: pointer">
+            <h5 style="cursor: pointer; font-weight: bold">
                 <span class="text-info cursor" <?= ($model->description)?'data-toggle="tooltip" title="'.$model->description.'"':'';?>><?= $model->display_name; ?></span>
-            </h4>
+            </h5>
             <h5 style="font-weight: bolder">Flamez Cash: <span class="text-success"><?= ($model->cash == -1)?'N/A':$model->cash; ?></span></h5>
             <h5 style="font-weight: bolder">Flamez Coins: <span class="text-warning"><?= ($model->coin == -1)?'N/A':$model->coin; ?></span></h5>
         </div>
