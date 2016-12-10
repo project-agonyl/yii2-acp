cd6a6b56e87c42793e63b5801348d2b679ab6748
Karthik P <karthikreadnovel@gmail.com>
2016-12-10 15:31:39 +0530

    Latest PK show fix

--- frontend/views/dashboard/_playerKill.php
+++ frontend/views/dashboard/_playerKill.php
@@ -11,7 +11,7 @@ use yii\helpers\Url;
     <center style='color: green'>
         <h5 style="font-weight: bold">
             <?= $model->pker ?>(<?= $model->pker_rb ?>, <?= $model->pker_lvl ?>) of <?= (strtolower(trim($model->pker_nation)) == 't')?'<font color="red">Temoz</font>':'<font color="skyblue">Quanato</font>'; ?> killed
-            <?= $model->pked ?>(<?= $model->pked_rb ?>, <?= $model->pked_lvl ?>) of <?= (strtolower(trim($model->pker_nation)) == 't')?'<font color="red">Temoz</font>':'<font color="skyblue">Quanato</font>'; ?> at
+            <?= $model->pked ?>(<?= $model->pked_rb ?>, <?= $model->pked_lvl ?>) of <?= (strtolower(trim($model->pked_nation)) == 't')?'<font color="red">Temoz</font>':'<font color="skyblue">Quanato</font>'; ?> at
             <?= $model->loc; ?>
         </h5>
     </center>
