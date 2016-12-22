7cee538117871af597f320625d4f2d3efcf0b17b
Karthik P <karthik.p@talview.com>
2016-12-22 15:23:50 +0530

    Added orange crafting to rb crafting guide

--- frontend/views/guides/rebirthCrafting.php
+++ frontend/views/guides/rebirthCrafting.php
@@ -18,6 +18,17 @@
             <tbody>
             <tr>
                 <td>Ur(1) Thorn(2) Ken(3)</td>
+                <td>H</td>
+                <td>A</td>
+                <td>L</td>
+                <td>O</td>
+                <td>Upgrade Jewel</td>
+                <td>-</td>
+                <td>Mandarin Oranges</td>
+                <td>No</td>
+            </tr>
+            <tr>
+                <td>Ur(1) Thorn(2) Ken(3)</td>
                 <td>Mandarin Oranges</td>
                 <td>Mandarin Oranges</td>
                 <td>Upgrade Jewel</td>
